c44c1c5559a32efc70ef6b5c805f14688b8bb363
maishujie <maishujie@aliyun.com>
2018-12-04 17:16:22 +0800

    125

---
+++ zixue/k-jinlin.py
@@ -0,0 +1,12 @@
+"""先配置PyCharm的背景,快捷键组合,视图模式和风格
+使用快捷键Ctrl + "`"(该键位于键盘的左上角, 和”~“同一个键位), 即可开发上图配置选项。
+color Scheme: 配置背景颜色
+code style  scheme: 默认为default
+View mode: 视图, 分为3种, 依人而定
+后设置字体大小
+因为习惯了使用notepad++等工具的Ctrl+鼠标滚轮的缩放字体功能,需要在PyCharm中开启。具体方法如下:
+File --> Setting --> Editor --> General --> 勾选Change font size (zoom) with Ctrl+Mouse Wheel
+设置默认字体大小:
+File --> Setting --> Editor --> Color&;Font -->Font-->Copy Scheme 继续设置,然后save。(PyCharm自带的scheme不能被改动,只能copy一份再改)
+"""
+import sklearn
