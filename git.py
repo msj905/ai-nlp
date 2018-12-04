6114b8477fffec9bc440a629ee541230721f3783
maishujie <maishujie@aliyun.com>
2018-12-04 17:09:34 +0800

    124

---
+++ git.py
@@ -0,0 +1,29 @@
+"""
+git clone https://github.com/msj905/ai-nlp.git
+cd ai-nlp/
+#
+echo "# ai-nlp" >> README.
+
+git add .
+
+
+ssh-keygen -t rsa -C "maishujie@aliyun.com"
+
+
+id_rsa.pub添加到github账号的设置里
+
+
+git config --global user.email "maishujie@aliyun.com"
+
+
+git config --global user.name "maishujie"
+
+
+git commit -m "123"
+
+
+
+git push -u origin master
+
+
+"""
